7d693f3662d3bf5af053e23ccc8cd18e614accd9
‪Ahmed Zaky‬‏ <103897664+ahmedzaky1@users.noreply.github.com>
2023-11-11 11:36:24 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="app">
+    <div class="header container h-100 p-5">
+      <h1 class="mb-5">Weather App</h1>
+      <div class="d-flex justify-content-center h-100">
+        <div class="searchbar mx-2 w-50">
+          <input
+            type="text"
+            class="input form-control"
+            v-model="city"
+            placeholder="Enter a City"
+          />
+        </div>
+        <button class="btn-search btn btn-primary" @click="searchWeather">
+          Search <i class="fas fa-search"></i>
+        </button>
+      </div>
+    </div>
+    <br />
+    <weather-component :city="city" v-if="showWeather"></weather-component>
+  </div>
+</template>
+
+<script>
+import WeatherComponent from "./components/WeatherComponent.vue";
+
+export default (await import("vue")).defineComponent({
+  name: "App",
+  data() {
+    return {
+      city: "",
+      showWeather: false,
+    };
+  },
+  methods: {
+    async searchWeather() {
+      this.searchWeather = false;
+      await this.$nextTick();
+      this.showWeather = true;
+    },
+  },
+  components: { WeatherComponent },
+});
+</script>
+
+<style>
+body {
+  background-color: #121212 !important;
+}
+.header {
+  background-color: #212730 !important;
+  border-radius: 20px;
+  color: #fff;
+  text-align: center;
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  margin-top: 5rem;
+}
+.btn-search {
+  background-image: linear-gradient(to right, cyan, magenta);
+}
+.btn-search span {
+  margin-right: 7px !important;
+}
+</style>
---
+++ src/components/DaysWeather.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="days-tab text-center">
+    <div v-if="loading" class="loading">Loading...</div>
+    <ul v-else class="p-0">
+      <li v-for="day in forecast" :key="day.date" class="li_active">
+        <div class="py-3"><img :src="day.iconUrl" /></div>
+        <div class="py-3">{{ day.description }}</div>
+        <div class="py-3">{{ day.temperature }} &deg;C</div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import moment from "moment";
+export default (await import("vue")).defineComponent({
+  props: {
+    cityName: String,
+  },
+  data() {
+    return {
+      forecast: [],
+      loading: true,
+      iconUrl: null,
+      date: null,
+    };
+  },
+  mounted() {
+    this.fetchWeatherData();
+  },
+  methods: {
+    async fetchWeatherData() {
+      console.log(this.cityName, "Working");
+      const apikey = "48c63522a57ecd2af48aa88228fbc80c";
+      const city = this.cityName;
+      const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apikey}`;
+      await axios
+        .get(apiUrl)
+        .then((Response) => {
+          const forecastData = Response.data.list;
+          const filterdDtat = forecastData
+            .map((item) => {
+              return {
+                data: moment(item.dt_txt.split("  ")[0]),
+                temperature: Math.round(item.main.temp),
+                description: item.weather[0].description,
+                iconUrl: `https://api.openweathermap.org/img/w/${item.weather[0].icon}.png`,
+              };
+            })
+            .reduce((acc, item) => {
+              if (!acc.some((day) => day.date == (item.date, "day"))) {
+                acc.push(item);
+              }
+              return acc;
+            }, [])
+            .slice(1, 5);
+          console.log(filterdDtat, "working");
+          this.forecast = filterdDtat;
+          this.loading = false;
+        })
+        .catch((error) => {
+          console.error("Error fetching weather date:", error);
+          this.loading = false;
+        });
+    },
+    // getDayName() {
+      
+    // },
+  },
+});
+</script>
+
+<style>
+.days-tab {
+  width: 90%;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  border-radius: 20px;
+  margin: auto;
+}
+.loading {
+  color: white;
+}
+.ul {
+  margin: 0;
+}
+li {
+  display: inline-block;
+  list-style: none;
+  height: 100%;
+  width: 21%;
+  max-width: 21%;
+  font-size: 1vw;
+  line-height: 1.2;
+}
+span {
+  display: block;
+  margin-bottom: 5px;
+  font: 100% sans-serif;
+  height: 35px;
+}
+.li_active {
+  background: #253d5c;
+  color: #222831;
+  border-radius: 10px;
+  margin: 0.5rem;
+  color: #fff;
+  font-weight: 600;
+}
+.li_active:hover {
+  transform: scale(1.2);
+  transition: transform 0.1s ease;
+}
+.li_active_temp {
+  display: inline-block;
+  background-color: #222831;
+  color: #fff;
+  transition: background-color 0.5s;
+  border-radius: 10px;
+}
+.li_active_temp:hover {
+  transform: scale(1.2);
+  transition: transform 0.1s ease;
+  background: #fff;
+  border-radius: 10px;
+  color: #191a1f;
+}
+</style>
